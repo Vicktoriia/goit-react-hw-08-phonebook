9ba4d1dfeca15cb08588eca1d38900dfd0440997
Viktoriia Aleksieieva <105809872+Vicktoriia@users.noreply.github.com>
2023-02-14 14:10:52 +0200

    add route

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from '../redux/operations';
-import  Phonebook  from '../Pages/Phonebook';
+import Phonebook from '../Pages/Phonebook';
 
- const App = () => {
-   const dispatch = useDispatch();
+const App = () => {
+  const dispatch = useDispatch();
 
-   useEffect(() => {
-     dispatch(fetchContacts());
-   }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
-   return (
-     <>
-       <Phonebook />
-     </>
-   );
- };
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Phonebook />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
